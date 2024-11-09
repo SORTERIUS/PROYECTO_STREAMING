8848c4e35c0a9c7492296e86bf1eae62bbda8dc7
SORTERIUS <espinozaismael9@gmailcom>
2024-11-08 20:59:46 -0500

    Borrador_1

---
+++ MAIN.go
@@ -0,0 +1,13 @@
+/*
+@autores: Henry / Ismael Espinoza
+@fecha: 08/11/2024
+@descripcion: programa de streaming de musica
+*/
+package main
+import (
+	fmt
+)
+
+func main (){
+	fmt.Println("todo listo")
+}
